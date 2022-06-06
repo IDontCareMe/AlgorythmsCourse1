4e99a62446bcbe2f995b9772a384048ae0661c04
Nikolai Voronin <54110187+IDontCareMe@users.noreply.github.com>
2022-06-06 19:47:28 +0000

    function fibanocci2 has added

--- ch01Fibanocci/main.go
+++ ch01Fibanocci/main.go
@@ -11,7 +11,8 @@ func main() {
     printError(err)
     os.Exit(1)
   }
-  fmt.Println(fibanocci1(n))
+  //fmt.Println(fibanocci1(n))
+  fmt.Println(fibanocci2(n))
 }
 
 func inputInt()(int, error) {
@@ -24,6 +25,7 @@ func printError(err error) {
   fmt.Println(err)
 }
 
+//this function returns n number fibanocci sequence
 func fibanocci1(n int)int {
   if n <=1 {
     return n
@@ -34,6 +36,22 @@ func fibanocci1(n int)int {
   )
   for i := 2; i <= n; i++ {
     first, second = second, first + second
+  }
+  return second
+}
+
+// this function returns last character
+//of n number fibanocci 
+func fibanocci2(n int)int {
+  if n <= 1 {
+    return n
+  }
+  var (
+    first int = 0
+    second int = 1
+  )
+  for i := 2; i <= n; i++ {
+    first, second = second, (first + second)%10
   }
   return second
 }
