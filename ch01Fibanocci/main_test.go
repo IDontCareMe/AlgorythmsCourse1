6e8706c49f820614e223032e61a87b49651b26f4
Nikolai Voronin <54110187+IDontCareMe@users.noreply.github.com>
2022-06-06 19:59:49 +0000

    new test for fibanocci2 has added

--- ch01Fibanocci/main_test.go
+++ ch01Fibanocci/main_test.go
@@ -25,3 +25,25 @@ func TestFibanocci(t *testing.T) {
     })
   }
 }
+
+func TestFibanocci2(t *testing.T) {
+  var tests = []struct {
+    n int
+    want int
+  } {
+    {0, 0},
+    {1, 1},
+    {8, 1},
+    {20, 5},
+    {841645, 5},
+  }
+  for _,test := range tests {
+    name := fmt.Sprintf("case %d", test.n)
+    t.Run(name, func(t *testing.T) {
+      got := fibanocci2(test.n)
+      if got != test.want {
+        t.Errorf("want:%d; got:%d", test.want, got)
+      }
+    })
+  }
+}
