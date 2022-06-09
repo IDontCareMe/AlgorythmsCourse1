c370b375e88d1b31af493965649cf06c172f1cfe
Nikolai Voronin <54110187+IDontCareMe@users.noreply.github.com>
2022-06-09 13:28:05 +0000

    Add sort library function from "sort". test.txt was created for debug //TODO: improve and add the tests and benchmarks

--- ch02Greed1/main.go
+++ ch02Greed1/main.go
@@ -4,12 +4,22 @@ import (
   "fmt"
   "os"
   "errors"
+  "sort"
 )
 
 // line structure
 type line struct {
   left, right int  
 }
+func (l *line) String()string {
+  return fmt.Sprintf("Left: %d, Right: %d", l.left, l.right)
+}
+
+// byLeft implements sort.Interface for []line based on the left field
+type byLeft []line
+func (b byLeft)Len()int { return len(b) }
+func (b byLeft)Less(i,j int)bool { return b[i].left < b[j].left }
+func (b byLeft)Swap(i,j int) { b[i], b[j] = b[j], b[i] }
 
 func main() {
   // read input
@@ -18,9 +28,10 @@ func main() {
     writeError(err)
   }
   // sort slice
-  lines = sortSlice(lines)
+  //lines = sortSlice(lines)
+  sort.Sort(byLeft(lines))
   n, points := findPoints(lines)
-  //output
+  // output
   fmt.Println(n)
   for _, l := range points {
     fmt.Printf("%d ",l)
@@ -29,7 +40,7 @@ func main() {
 
 // this functions reads input
 func readInput()(lines []line, err error){
-  //Get number of lines
+  // Get the number of lines
   var n int
   _, err = fmt.Scan(&n)
   if err != nil {
@@ -38,14 +49,14 @@ func readInput()(lines []line, err error){
   if n <= 0 {
     err = errors.New("error! The number of lines must be a number, greater than zero.")
   }
-  //Create slice of lines & fill it
+  // Create slice of lines & fill it
   lines = make([]line, n)
   for i,_ := range lines {
     _,err = fmt.Scan(&lines[i].left, &lines[i].right)
     if err != nil {
       return []line{}, err
     }
-    //Swap ends of line if necessary
+    // Swap ends of line if necessary
     if lines[i].left > lines[i].right {
       lines[i].left, lines[i].right = lines[i].right, lines[i].left
     }
@@ -53,6 +64,7 @@ func readInput()(lines []line, err error){
   return lines, err
 }
 
+/*
 //this function sorts slice (I don't know algorithm for sorting yet exept bubble sorting)
 //sorting by left end of the line
 //Think, it will be O(n^2) and it is bad((
@@ -70,7 +82,7 @@ func sortSlice(inputSlice []line)([]line) {
       return inputSlice
     }
   }
-}
+}*/
 
 // this function finds common points in line's slice
 func findPoints(lines []line)(n int, output []int) {
@@ -81,28 +93,29 @@ func findPoints(lines []line)(n int, output []int) {
   }
   
   for {
-    //find least right border for iteration
+    // find least right border for iteration
     for i:= curNum; i < len(lines); i++ {
       if minBorder >= lines[i].right {
         minBorder = lines[i].right
         curNum = i
       }
     }
-    //find all lines with left border less than new min border
-    for i := curNum; i < len(lines); i++ {
+    // first point has found
+    n++
+    output = append(output, minBorder)
+    // find all lines with left border less than new min border
+    for i := curNum + 1; i < len(lines); i++ {
       if minBorder >= lines[i].left {
         curNum = i
       }
     }
-    //first point has found
-    n++
-    output = append(output, minBorder)
-    //if all lines are covered, then return
+    // if all lines are covered, then return
     if curNum >= len(lines) - 1 {
       return
     }
-    //new min border is next line border. Get it and go to another step
-    minBorder = lines[curNum + 1].right
+    // new min border is next line border. Get it and go to another step
+    curNum++
+    minBorder = lines[curNum].right
   }
 }
 
