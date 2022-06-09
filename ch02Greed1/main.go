78c082ceb35601a50b64efb333d7df937fae68fb
Nikolai Voronin <54110187+IDontCareMe@users.noreply.github.com>
2022-06-09 08:09:39 +0000

    Works, but slow((

--- ch02Greed1/main.go
+++ ch02Greed1/main.go
@@ -6,6 +6,7 @@ import (
   "errors"
 )
 
+// line structure
 type line struct {
   left, right int  
 }
@@ -17,10 +18,12 @@ func main() {
     writeError(err)
   }
   // sort slice
-
+  lines = sortSlice(lines)
+  n, points := findPoints(lines)
   //output
-  for _, l := range lines {
-    fmt.Println(l.left, l.right)
+  fmt.Println(n)
+  for _, l := range points {
+    fmt.Printf("%d ",l)
   }
 }
 
@@ -33,7 +36,7 @@ func readInput()(lines []line, err error){
     return []line{}, err
   }
   if n <= 0 {
-    err = errors.New("error! The number of lines must be a number greater than zero.")
+    err = errors.New("error! The number of lines must be a number, greater than zero.")
   }
   //Create slice of lines & fill it
   lines = make([]line, n)
@@ -48,6 +51,59 @@ func readInput()(lines []line, err error){
     }
   }
   return lines, err
+}
+
+//this function sorts slice (I don't know algorithm for sorting yet exept bubble sorting)
+//sorting by left end of the line
+//Think, it will be O(n^2) and it is bad((
+func sortSlice(inputSlice []line)([]line) {
+  // bubble sorting
+  for {
+    var changeFlag bool = false
+    for i := 0; i < len(inputSlice) - 1; i++ {
+      if inputSlice[i].left > inputSlice[i+1].left {
+        inputSlice[i],inputSlice[i+1] = inputSlice[i+1], inputSlice[i]
+        changeFlag = true
+      }
+    }
+    if changeFlag == false {
+      return inputSlice
+    }
+  }
+}
+
+// this function finds common points in line's slice
+func findPoints(lines []line)(n int, output []int) {
+  curNum := 0
+  minBorder := lines[curNum].right
+  if len(lines) == 0 {
+    return 0, []int{}
+  }
+  
+  for {
+    //find least right border for iteration
+    for i:= curNum; i < len(lines); i++ {
+      if minBorder >= lines[i].right {
+        minBorder = lines[i].right
+        curNum = i
+      }
+    }
+    //find all lines with left border less than new min border
+    for i := curNum; i < len(lines); i++ {
+      if minBorder >= lines[i].left {
+        curNum = i
+      }
+    }
+    //first point has found
+    n++
+    output = append(output, minBorder)
+    //if all lines are covered, then return
+    if curNum >= len(lines) - 1 {
+      return
+    }
+    //new min border is next line border. Get it and go to another step
+    minBorder = lines[curNum + 1].right
+  }
 }
 
 // this function handles errors
