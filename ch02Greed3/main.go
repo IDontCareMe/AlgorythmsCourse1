01f47d184b3dab33d42f10dd14b7c6388a8734e1
Nikolai Voronin <54110187+IDontCareMe@users.noreply.github.com>
2022-06-10 21:16:25 +0000

    Add printRrsilt function. Complit findMany function

--- ch02Greed3/main.go
+++ ch02Greed3/main.go
@@ -7,9 +7,18 @@ import (
 )
 
 func main() {
-  
+  // Read input
+  n, err := readInputInt()
+  if err != nil {
+    printError(err)
+  }
+  // Find manys of numbers
+  res := findMany(n)
+  // Print result
+  printResult(res)
 }
 
+// This function reads inpet and returns int and error(if is)
 func readInputInt()(n int, err error) {
   _, err = fmt.Scan(&n)
   if (err != nil) || (n == 0) {
@@ -18,6 +27,32 @@ func readInputInt()(n int, err error) {
   return
 }
 
+// This function calculates maximum manys of numbers with sum eq n
+func findMany(n int)(many []int) {
+  var i int = 1
+  for {
+    if i < (n - i) {
+      n -=i
+      many = append(many, i)
+    } else {
+      many = append(many, n)
+      break
+    }
+    i++
+  }
+  return
+}
+
+// This function printsvresult
+func printResult(res []int) {
+  fmt.Println(len(res))
+  for _, val := range res {
+    fmt.Printf("%d ", val)
+  }
+  fmt.Print("\n")
+}
+
+// This function handle and print errors
 func printError(err error) {
   fmt.Println(err)
   os.Exit(1)
