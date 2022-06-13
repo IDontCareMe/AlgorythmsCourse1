dcf4b25411e36f8edf0ed714163eb0eab363b02c
Nikolai Voronin <54110187+IDontCareMe@users.noreply.github.com>
2022-06-13 19:31:30 +0000

    Still not perfect((

--- ch02huffman1/main.go
+++ ch02huffman1/main.go
@@ -6,61 +6,18 @@ import(
   "os"
   "errors"
   "bufio"
+  "sort"
+  "strings"
 )
 
 // Tree node struct node for binar tree contains frq & char
 type TreeNode struct {
   frq int
   char rune
-  zero *TreeNode
-  one *TreeNode
 }
 func (t *TreeNode) String()string {
-  return fmt.Sprintf("Frq: %d, Char: %c, Zero: %x, One: %x", t.frq, t.char, t.zero, t.one)
+  return fmt.Sprintf("Frq: %d, Char: %c", t.frq, t.char)
 }
-
-// TreeQueue keep nodes for building a tree
-type TreeQueue []TreeNode
-// 
-func (t *TreeQueue) String()(s string) {
-  for _,val := range *t {
-    s += fmt.Sprintf("%s: %v\n",string(val.char), val.frq)
-  }
-  return
-}
-// This function appends Nodes from map
-func (t *TreeQueue) Create(m map[rune]int) {
-  // Create all leaves
-  for key,value := range m {
-    *t = append(*t, TreeNode{value, key, nil, nil})
-  }
-}
-// This function extracts Node from queue and returns it's adreess
-func (t *TreeQueue) ExtractMin()TreeNode {
-  min := (*t)[0].frq
-  num := 0
-  for i,v := range *t {
-    if min > v.frq {
-      min = v.frq
-      num = i
-    } 
-  }
-  // Store Node address
-  var p TreeNode = (*t)[num]
-  // Exclude Node from Queue
-  if num == len(*t) - 1 {
-    *t = (*t)[:num]  
-  } else {
-   *t = append((*t)[:num], (*t)[num+1:]...) 
-  }
-  
-  return p
-}
-// This function insert new node in queue
-func (t *TreeQueue)Insert(n TreeNode) {
-  *t = append(*t,n)
-}
-
 
 func main() {
   // Read input
@@ -70,35 +27,27 @@ func main() {
   }
   // search all runes in string
   res := searchRune(s)
-  // Create Queue
-  var tq TreeQueue = make([]TreeNode, 0)
-  tq.Create(res)
-  
-  /*var p TreeNode
-  for i:=0; i < 8; i++ {
-    p = tq.ExtractMin()
-    fmt.Println(p.String())
-  }*/
+  // Create code table
+  table := codeTable(res)
+  // Code string
+  answer := code(s, table)
   // print result
-  //printResult(res)
-  //
-  p:=createTree(tq)
-  fmt.Println(p)
+  printResult(answer, table)
 }
 
 // This function reads input and returns string of error
 func readInputString()(s string, err error) {
   reader := bufio.NewReader(os.Stdin)
   s, err = reader.ReadString('\n')
-  //_, err = fmt.Scan(&s)
     if err == io.EOF { err = nil }
   if len(s) <= 0 {
     err = errors.New("String must contains at least one char")
   }
+  s = strings.TrimSpace(s)
   return 
 }
 
-// This function searching symbols in srtings and returns count of thic symbol as map
+// This function searching symbols in srtings and returns count of it as a map
 func searchRune(s string)(result map[rune]int){
   result = make(map[rune]int)
   for _, value := range s {
@@ -107,35 +56,48 @@ func searchRune(s string)(result map[rune]int){
   return
 }
 
-// This function create binarry tree and and returns root node
-func createTree(tq TreeQueue)(node *TreeNode) {
-  //for i := 1; i < len(tq); i++ {
-  fmt.Println(tq.String(),"Queue lenght:",len(tq))
-  for {
-    if len(tq) <= 1 {return}
-    zero := tq.ExtractMin()
-    //fmt.Println("zero frq:",(*zero).frq)
-    fmt.Println("zero frq:", zero.String())
-    one := tq.ExtractMin()
-    fmt.Println("one frq:",one.String())
-    node = &TreeNode{
-      zero.frq + one.frq,
-      0,
-      &zero,
-      &one,
-    }
-    tq.Insert(*node)
-    fmt.Println("New node:",node)
-    fmt.Println(tq.String(),"Queue lenght:",len(tq))
+// This function returns sorted code table
+func codeTable(m map[rune]int)(table map[rune]string) {
+  // Create table as slice
+  var t []TreeNode = make([]TreeNode, 0)
+  for key, value := range m {
+    t = append(t, TreeNode {value, key})
   }
-  //return  
+  // Sort table slice
+  sort.Slice(t,func(i,j int)bool{
+    return t[i].frq > t[j].frq
+  })
+  // Code table
+  table = make(map[rune]string)
+  if len(t) <= 1 {
+    table[t[0].char] = "0"
+    return
+  }
+  for i,val := range t {
+    c := strings.Repeat("1",i)
+    if i != len(t)-1 {
+      c += "0"
+    }
+    table[val.char] = c
+  }
+  return
+}
+
+// This function codes input string
+func code(s string, t map[rune]string)(c string) {
+  for _,v := range s {
+    c += t[v] 
+  }
+  return
 }
 
 // This function prints result
-func printResult(m map[rune]int) {
-  for key, val := range m {
-    fmt.Println(string(key), val)
+func printResult(s string, t map[rune]string) {
+  fmt.Printf("%d %d\n",len(t), len(s))
+  for key, val := range t {
+    fmt.Printf("%s: %s\n",string(key), val)
   }
+  fmt.Println(s)
 }
 
 // This function handles errors
