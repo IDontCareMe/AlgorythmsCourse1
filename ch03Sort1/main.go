5c4ca5b1d8ed997f4499db081b8eeb23137f7329
Nikolai Voronin <54110187+IDontCareMe@users.noreply.github.com>
2022-06-19 21:27:02 +0000

    Update functions and tests

--- ch03Sort1/main.go
+++ ch03Sort1/main.go
@@ -11,7 +11,8 @@ func main() {
   if err != nil {
     printError(err)
   }
-  fmt.Println(unsorted)
+  _, inv := mergeSort(unsorted)
+  fmt.Println(inv)
 }
 
 // this function read os.Stdin and returns []int slice
@@ -31,18 +32,59 @@ func readInput()(unsorted []int, err error) {
   return
 }
 
-func merge(left,right []int)(merged []int) {
+// This function...
+func mergeSort(unsorted []int)([]int, int) {
+  if len(unsorted) > 1 {
+    m := len(unsorted)/2
+    l,i1 := mergeSort(unsorted[:m])
+    r, i2 := mergeSort(unsorted[m:])
+    sorted, n := merge(l, r, i1, i2)
+    //sorted, n := merge(mergeSort(unsorted[:m]), mergeSort(unsorted[m:]))
+    return sorted, n
+  }
+  return unsorted, 0
+}
+
+// This function merge two slices to one
+func merge(left,right []int, invl, invr int)(merged []int, n int) {
   merged = make([]int, len(left) + len(right))
   l, r := 0, 0
   for i, _ := range merged {
-    if l >= len(left) {
-      merged = right[r:]
-    }
-    if left[l] < right[r] {
+    if (l >= len(left)) && (r < len(right)) {
+      merged[i] = right[r]
+      r++
+    } else if (l < len(left)) && (r >= len(right)) {
       merged[i] = left[l]
       l++
+    } else if (l < len(left))&& (r < len(right)){
+      if left[l] < right[r] {
+        merged[i] = left[l]
+        l++
+      } else {
+        merged[i] = right[r]
+        r++
+      }
     }
   }
+  n = invl + invr + findInversionNumber(left, right)
+  //fmt.Println(n)
+  return
+}
+
+// This function finds all inversions
+func findInversionNumber(left, right []int)(n int) {
+  r := 0
+  for index, value := range left {
+    for i := r; i < len(right); i++ {
+      if value > right[i] {
+        n += len(left) - index
+        r++
+      } else {
+        break
+      }
+    } 
+  }
+  return
 }
 
 // This function handles errors
